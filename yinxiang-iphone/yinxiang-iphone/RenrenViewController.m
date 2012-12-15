2121fd60eefd659fdbb63c3ab36c8dfc3c80c9fd
He Jun <freefcw@gmail.com>
2012-12-15 21:53:20 +0800

    add friends in app

--- yinxiang-iphone/yinxiang-iphone/RenrenViewController.m
+++ yinxiang-iphone/yinxiang-iphone/RenrenViewController.m
@@ -9,10 +9,14 @@
 #import "RenrenViewController.h"
 
 @interface RenrenViewController ()
-
+{
+    NSArray        *peoples;
+}
 @end
 
 @implementation RenrenViewController
+
+@synthesize renren = _renren;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,6 +31,11 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    NSMutableDictionary *params = [NSMutableDictionary dictionaryWithCapacity:10];
+    [params setObject:@"friends.getAppFriends" forKey:@"method"];
+    [params setObject:@"name, tinyurl" forKey:@"fields"];
+    
+    [self.renren requestWithParams:params andDelegate:self];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +43,112 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [peoples count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure the cell...
+    id p = [peoples objectAtIndex: indexPath.row];
+    NSLog(@"kkkkk \n%@", p);
+    cell.textLabel.text = [(NSDictionary *)p objectForKey: @"name"];
+    
+    return cell;
+}
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ }
+ else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+ {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+}
+
+
+/**
+ * 接口请求成功，第三方开发者实现这个方法
+ */
+- (void)renren:(Renren *)renren requestDidReturnResponse:(ROResponse*)response
+{
+    //    NSLog(@"%@", response.rootObject);
+    peoples = response.rootObject;
+    
+    [self.tableView reloadData];
+}
+
+
+/**
+ * 接口请求失败，第三方开发者实现这个方法
+ */
+- (void)renren:(Renren *)renren requestFailWithError:(ROError*)error
+{
+    
+}
+
+
 
 @end
